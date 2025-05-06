8d80b4459a4c064d71c86500c796a9350a68fc23
ojasvidutt <ojasvidutt@gmail.com>
2025-05-06 11:59:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+let currentSong = new Audio();
+let playmain = document.querySelector(".playbar .play")
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let res = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = res;
+    let elements = div.getElementsByTagName("a");
+    let songs = [];
+    Array.from(elements).forEach(e => {
+
+        if (e.href.endsWith(".mp3") || e.href.endsWith(".m4a")) {
+            songs.push({
+                title: extractTitle(e.title),
+                href: e.href,
+            });
+        }
+
+    });
+    return songs;
+}
+
+async function main() {
+    let songs = await getSongs();
+    
+    
+   
+    songs.forEach((song) => {
+        let li = document.createElement("li");
+        li.innerHTML = `<i class="fa-solid fa-music"></i><span class="songtitle">${song.title}</span> <span class="link">${song.href}</span> <span class="play fa-solid fa-play"></span> `;
+        document.querySelector(".songlist").appendChild(li);
+    })
+
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach((i) => {
+        i.querySelector(".play").addEventListener("click", (e) => {
+            document.querySelector(".playbar").style.display="grid"
+            playMusic(i.querySelector(".link").innerHTML);
+            playmain.classList.add("fa-pause");
+            playmain.classList.remove("fa-play");
+           if(currentSong.paused){ e.target.classList.add("fa-pause");
+            e.target.classList.remove("fa-play");
+           }
+           else{
+            e.target.classList.add("fa-play");
+            e.target.classList.remove("fa-pause");
+           }
+          document.querySelector(".songinfo").textContent=i.querySelector(".songtitle").textContent;
+          
+            
+        })
+    })
+
+   playmain.addEventListener("click",function (e){
+        if(currentSong.paused){
+            currentSong.play();
+             e.target.classList.add("fa-pause");
+            e.target.classList.remove("fa-play");
+             }
+             else{
+                currentSong.pause();
+                e.target.classList.add("fa-play");
+                e.target.classList.remove("fa-pause");
+
+             }
+    })
+
+    currentSong.addEventListener("timeupdate", () => {
+        let currentTime = formatTime(currentSong.currentTime);
+        let duration = formatTime(currentSong.duration);
+        document.querySelector(".songtime").innerText=(`${currentTime}/${duration}`);
+        document.querySelector(".circle").style.left= (currentSong.currentTime/currentSong.duration)*100 + "%"
+
+
+
+    });
+
+    document.querySelector(".seekbar").addEventListener("click",(e)=>{
+        console.log(e.offsetX)
+  document.querySelector(".circle").style.left = (e.offsetX/e.target.getBoundingClientRect().width)*100 + "%";
+  currentSong.currentTime= (currentSong.duration/e.target.getBoundingClientRect().width) * e.offsetX; 
+
+    })
+
+    function formatTime(seconds) {
+        if (isNaN(seconds)) return "00:00";
+        let minutes = Math.floor(seconds / 60);
+        let secs = Math.floor(seconds % 60);
+        return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+    }
+}
+
+
+main();
+
+function extractTitle(ogtitle) {
+    let step1 = ogtitle.split(".")[0];
+    return step1.split("-").join(" ");
+}
+
+function playMusic(track) {
+    currentSong.src = track;
+    currentSong.play();
+    
+}
